28b7d47e313828c561e98d72ada87f9e6c03388b
Tushar <tushar@workongrid.com>
2024-12-29 23:18:40 +0530

    people name automater

--- nodemailer.js
+++ nodemailer.js
@@ -50,7 +50,7 @@ const sendEmail = async (row) => {
 I'm actively seeking Senior Frontend opportunities where I can continue to make a meaningful impact. I'd love to discuss how my skills and experiences align with ${Company}.
 </p>
 <p>You can view my portfolio <a href="https://tushar.vercel.app">tushar.vercel.app</a> and GitHub projects <a href="https://github.com/tushartiwari7">here</a>.</p>
-<p>Currently, I am <b>serving notice period</b> and can <b>join within 15 days</b> of receiving an offer. A little help from your side can significantly help my career.</p>
+<p>Currently, I am <b>serving notice period</b> with an offer from <b>Flipkart Group</b>. A little help from your side can significantly help my career.</p>
 <p>PS: I have attached my <b><a href="https://drive.google.com/file/d/1AQ2bZ_yUheXoKPHwcAgyQIZxWAO_NwMT/view?usp=sharing">Resume</a></b> & <b><a href="https://www.linkedin.com/in/tushartiwari7/">Linkedin Profile</a></b> ${
       Link !== undefined ? `& <b><a href=${Link}>Job Opening</a> </b>` : ""
     } for you to take a look at. If you find me suitable, please help me with an Interview Opportunity at ${Company}.</p>
---
+++ people.js
@@ -0,0 +1,35 @@
+const peoples = require("./peoples.json");
+const xlsx = require("xlsx");
+
+const NAME_COLUMN_INDEX = 0;
+const EXCEL_FIRST_ROW = 10;
+// Load your Excel file
+const workbook = xlsx.readFile("./referrals.xlsx"); // Path for the sheet in your local folder
+const sheetName = "references"; // Change to the name of your sheet
+const worksheet = workbook.Sheets[sheetName];
+// Convert the sheet to JSON to manipulate rows easily
+const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 }); // header: 1 for array of arrays
+
+// Update specific column for multiple rows
+const columnToUpdate = NAME_COLUMN_INDEX; // Column index (e.g., 2 for the third column, as it's 0-based)
+// Update rows
+let index = EXCEL_FIRST_ROW;
+peoples.forEach((user) => {
+  if (user && user.name) {
+    if (jsonData[index]) jsonData[index][columnToUpdate] = user.name;
+    else {
+      jsonData[index] = [null,null,null,null,null,null];
+      jsonData[index][columnToUpdate] = user.name;
+    }
+    index++;
+  }
+});
+
+// Convert JSON back to sheet
+const updatedWorksheet = xlsx.utils.aoa_to_sheet(jsonData);
+
+// Replace the sheet in the workbook
+workbook.Sheets[sheetName] = updatedWorksheet;
+
+// Save the updated file
+xlsx.writeFile(workbook, 'referrals.xlsx'); // Replace with desired output path
